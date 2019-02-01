458448b20ea4064be6e511d9b26d04e6b3a15af1
ponolac <ponolac@gmail.com>
2019-02-01 01:05:55 -0800

    var, let, const overview

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
-console.time('Testing123');
-  console.log('some tests');
-console.timeEnd('Testing123');
+// var, let, const
+
+var name = 'John Doe'; // declared a variable
+console.log(name); // logs it into console
+
+// can reassign a var value with var or let, but not const.
+
+name = "Jim Unknown";
+console.log(name);
+
+// init var
+
+var greeting; // undefined
+console.log(greeting);
+greeting = 'Hello';
+console.log(greeting);
+
+// var's can only include: letters, numbers, _, $
+// cannot start with a number
