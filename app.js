10f56ac267fd7219f6811eb1f4a68c56264138b1
ponolac <ponolac@gmail.com>
2019-02-03 03:03:15 -0800

    converting number to a string

--- app.js
+++ app.js
@@ -1,67 +1,13 @@
-// // var, let, const
-
-// var name = 'John Doe'; // declared a variable
-// console.log(name); // logs it into console
-
-// // can reassign a var value with var or let, but not const.
-
-// name = "Jim Unknown";
-// console.log(name);
-
-// // init var
-
-// var greeting; // undefined
-// console.log(greeting);
-// greeting = 'Hello';
-// console.log(greeting);
-
-// // var's can only include: letters, numbers, _, $
-// // cannot start with a number
+// TYPE CONVERSION
 
 
-// LET
-// let name = 'John Doe'; 
-// console.log(name); 
-// name = "Jim Unknown";
-// console.log(name);
 
-// CONST -- always constant, cannot change.
-// const name1 = "Jim";
-// console.log(name1);
-// CONST must have value.
+// Converting number to string
 
-// PRIMITIVE TYPES
+let val;
 
-// String
-const name = 'Jim Doe';
+val = String(5); // wrap in String function
 
-// Number
-const age = 50;
-
-// Boolean
-const hasCar = false;
-
-// Null
-const car = null; // returns an object
-
-// Undefined
-let test;
-
-// Symbol -> new for ES6
-const sym = Symbol();
-
-// =========================================
-
-// REFERENCE TYPES - Objects
-// Array
-const vehicles = ['cars', 'trucks'];
-
-// Object Literal
-const address = {
-  city: 'Boston',
-  state: 'MA'
-}
-
-const today = new Date();
-console.log(today);
-console.log(typeof today);
+console.log(val);
+console.log(typeof val);
+console.log(val.length);
