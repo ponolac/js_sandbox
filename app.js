88c58f06ce64307d6f7486a86804812b9341eac3
ponolac <ponolac@gmail.com>
2019-02-04 20:03:19 -0800

    converting numbers to strings with toString() method

--- app.js
+++ app.js
@@ -3,7 +3,10 @@
 let val;
 
 // Converting number to string
-val = String(5); // wrap in String function
+
+// One option is to wrap in string function
+
+val = String(5);
 val = String(4+4);
 
 // Converting boolean to string
@@ -14,6 +17,13 @@ val = String(new Date());
 
 // Converting array to string
 val = String([1,2,3,4]);
+
+// Another option is the toString() method
+val = (5).toString();
+val = (false).toString();
+val = (new Date()).toString();
+val = (5*5).toString();
+val = ([1,2,3,4]).toString();
 
 // Output
 console.log(val);
