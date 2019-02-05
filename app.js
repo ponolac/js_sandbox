00df3e71198754ac3ef3f5114c3c489e732fc089
ponolac <ponolac@gmail.com>
2019-02-05 01:44:59 -0800

    properties and methods available with strings

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const firstName = 'William';
 const lastName = 'Johnson';
 const age = 22;
+const str = 'Hullo derr my name is Test';
+const tags = 'web dev,programming,testing';
 
 let val;
 
@@ -17,5 +19,49 @@ val = 'Hello, my name is ' + firstName + ' and I am ' + age + '.';
 
 // Escaping
 val = 'That\'s great, I can\'t wait';
+
+
+
+
+//   PROPERTIES & METHODS AVAILABLE WITH STRINGS
+
+// length property
+val = firstName.length;
+
+// concat() method
+val = firstName.concat(' ', lastName);
+
+// change case method
+val = firstName.toUpperCase();
+val = firstName.toLowerCase();
+
+val = firstName[5]; // can treat strings like read-only arrays
+
+// indexOf() method
+val = firstName.indexOf('l');
+val = firstName.lastIndexOf('a');
+
+// charAt()
+val = firstName.charAt('5');
+
+// get last char formula
+val = firstName.charAt(firstName.length - 1);
+
+// substring()
+val = firstName.substring(0, 4);
+
+// slice()
+val = firstName.slice(0, 4);
+val = firstName.slice(-3);
+
+// split()
+val = str.split(' ');
+val = tags.split(',');
+
+// replace()
+val = str.replace('Jack', 'Test');
+
+// includes()
+val = str.includes('Hullo');
 
 console.log(val);
