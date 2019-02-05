82619e300338b2c81db0cec41df81a3801941b7e
ponolac <ponolac@gmail.com>
2019-02-05 02:31:05 -0800

    template literals / template strings in es5 example

--- app.js
+++ app.js
@@ -1,67 +1,18 @@
-const firstName = 'William';
-const lastName = 'Johnson';
-const age = 22;
-const str = 'Hullo derr my name is Test';
-const tags = 'web dev,programming,testing';
+// TEMPLATE LITERALS / template strings. ES6
 
-let val;
+const name = 'John';
+const age = 23;
+const job = 'Dev';
+const city = 'Miami';
+let html;
 
-val = firstName + lastName;
+// Without template strings (es5)
+html = '<ul><li>Name: ' + name + '</li><li>Age: ' + age + '</li><li>Job: ' + job + '</li><li>City: ' + city + '</li></ul>';
 
-// Concatenation
-val = firstName + ' ' + lastName;
+html = '<ul>' +
+          '<li>Name: ' + name + '</li>' +
+          '<li>Age: ' + age + '</li>' +
+          '<li>Job: ' + job + '</li>' +
+          '<li>City: ' + city + '</li>';
 
-// Append
-val = 'Buddy ';
-val += 'Holland';  // += to append
-
-val = 'Hello, my name is ' + firstName + ' and I am ' + age + '.';
-
-// Escaping
-val = 'That\'s great, I can\'t wait';
-
-
-
-
-//   PROPERTIES & METHODS AVAILABLE WITH STRINGS
-
-// length property
-val = firstName.length;
-
-// concat() method
-val = firstName.concat(' ', lastName);
-
-// change case method
-val = firstName.toUpperCase();
-val = firstName.toLowerCase();
-
-val = firstName[5]; // can treat strings like read-only arrays
-
-// indexOf() method
-val = firstName.indexOf('l');
-val = firstName.lastIndexOf('a');
-
-// charAt()
-val = firstName.charAt('5');
-
-// get last char formula
-val = firstName.charAt(firstName.length - 1);
-
-// substring()
-val = firstName.substring(0, 4);
-
-// slice()
-val = firstName.slice(0, 4);
-val = firstName.slice(-3);
-
-// split()
-val = str.split(' ');
-val = tags.split(',');
-
-// replace()
-val = str.replace('Jack', 'Test');
-
-// includes()
-val = str.includes('Hullo');
-
-console.log(val);
+document.body.innerHTML = html;
