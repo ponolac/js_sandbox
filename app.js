f07210f88915f81d09589f7f3c173f452283dbbe
ponolac <ponolac@gmail.com>
2019-02-04 20:37:03 -0800

    convert strings to numbers using number function (with .toFixed method)

--- app.js
+++ app.js
@@ -3,10 +3,9 @@
 let val;
 
 // Converting number to string
-
 // One option is to wrap in string function
 
-val = String(5);
+val = String(5); 
 val = String(4+4);
 
 // Converting boolean to string
@@ -25,7 +24,13 @@ val = (new Date()).toString();
 val = (5*5).toString();
 val = ([1,2,3,4]).toString();
 
+// Converting strings to numbers
+// One option is the number function
+
+val = Number('5');
+
 // Output
 console.log(val);
 console.log(typeof val);
-console.log(val.length); // .length property only works on strings
+// console.log(val.length); // .length property only works on strings
+console.log(val.toFixed(1)); // toFixed method only works on numbers and allows you to specify decimals.
