b9f562f11965caa71acd5d912df18e6437a8c40b
ponolac <ponolac@gmail.com>
2019-02-05 02:42:33 -0800

    template literals with es6

--- app.js
+++ app.js
@@ -13,6 +13,25 @@ html = '<ul>' +
           '<li>Name: ' + name + '</li>' +
           '<li>Age: ' + age + '</li>' +
           '<li>Job: ' + job + '</li>' +
-          '<li>City: ' + city + '</li>';
+          '<li>City: ' + city + '</li>' +
+          '</ul>';
+
+function hello(){
+  return 'hello';
+}
+
+// With template literals / template strings (es6)
+
+html = `
+  <ul>
+    <li>Name: ${name}</li> 
+    <li>Age: ${age}</li>
+    <li>Job: ${job}</li>
+    <li>City: ${city}</li>
+    <li>${2 + 2}</li>
+    <li>${hello()}</li>
+    <li>${age > 30 ? 'Over 30' : 'Under 30'}</li>
+  </ul>
+`;
 
 document.body.innerHTML = html;
