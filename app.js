20852dd0cd992673d4721393eeff8f7c497d69ab
ponolac <ponolac@gmail.com>
2019-02-04 19:34:29 -0800

    converting array to string

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
-// TYPE CONVERSION
-// taking a variable and changing the data type
-
-
-// Converting number to string
+// TYPE CONVERSION -- taking a variable and changing the data type.
 
 let val;
 
+// Converting number to string
 val = String(5); // wrap in String function
 val = String(4+4);
 
@@ -15,8 +12,10 @@ val = String(true);
 // Converting date to string
 val = String(new Date());
 
+// Converting array to string
+val = String([1,2,3,4]);
+
 // Output
 console.log(val);
 console.log(typeof val);
 console.log(val.length); // .length property only works on strings
-
