b22c044005fe77bff09477d77707baf57100a156
ponolac <ponolac@gmail.com>
2019-02-06 02:01:01 -0800

    function declarations es5 & es6

--- app.js
+++ app.js
@@ -1,41 +1,11 @@
-const color = 'red';
+// Function declarations
 
-switch(color){
-  case 'red':
-    console.log('Color is red');
-    break;
-  case 'blue': 
-  console.log('Color is blue');
-  break;
-  default: 
-    console.log('Color is not red or blue');
-    break;
+function greet(firstName = 'John', lastName = 'Doe'){
+  // es5 way of defining defaults:
+  // if(typeof firstName === 'undefined'){firstName = 'John'}
+  // if(typeof lastName === 'undefined'){lastName = 'Doe'}
+  // console.log('Hello');
+  return 'Hello ' + firstName + ' ' + lastName;
 }
 
-let day;
-
-switch(new Date().getDay()){
-  case 0:
-  day = 'Sunday';
-  break;
-    case 1:
-    day = 'Monday';
-    break;
-      case 2:
-      day = 'Tuesday';
-      break;
-        case 3:
-        day = 'Wednesday';
-        break;
-          case 4:
-          day = 'Thursday';
-          break;
-            case 5:
-            day = 'Friday';
-            break;
-              case 6:
-              day = 'Saturday';
-              break;
-}
-
-console.log(`Today is ${day}`);
+console.log(greet());
