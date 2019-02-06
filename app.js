a85a800dc0521b88a83759dab022514ca75fda63
ponolac <ponolac@gmail.com>
2019-02-06 03:13:09 -0800

    function expressions, IIFEs, property methods

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Function declarations
+// FUNCTION DECLARATIONS
 
 function greet(firstName = 'John', lastName = 'Doe'){
   // es5 way of defining defaults:
@@ -8,4 +8,43 @@ function greet(firstName = 'John', lastName = 'Doe'){
   return 'Hello ' + firstName + ' ' + lastName;
 }
 
-console.log(greet());
+// console.log(greet());
+
+// FUNCTION EXPRESSIONS (puts a function as a variable assignment)
+
+const square = function(x){
+  return x*x;
+};
+
+// console.log(square(8));
+
+// IMMEDIATELY INVOKABLE FUNCTION EXPRESSIONS - IIFE's ( a function you declare & run at same time)
+
+// (function(){
+//   console.log('IIFE ran...');
+// })();
+
+// (function(name){
+//   console.log('Hello '+ name);
+// })('Person'); 
+
+// PROPERTY METHODS
+
+// when a function is put inside of an object, it's called a method.
+
+const todo = {
+  add: function(){
+    console.log('Add todo...');
+  },
+  edit: function(id){
+    console.log(`Edit todo ${id}`);
+  }
+}
+
+todo.delete = function(){
+  console.log('Delete todo...');
+}
+
+todo.add();
+todo.edit(22);
+todo.delete();
