d39f8e62a89b78b82c9df7c8fffbfb92370b72ec
ponolac <ponolac@gmail.com>
2019-02-06 22:52:53 -0800

    for loop; continue; break;

--- app.js
+++ app.js
@@ -1,50 +1,15 @@
-// FUNCTION DECLARATIONS
+// FOR LOOP
 
-function greet(firstName = 'John', lastName = 'Doe'){
-  // es5 way of defining defaults:
-  // if(typeof firstName === 'undefined'){firstName = 'John'}
-  // if(typeof lastName === 'undefined'){lastName = 'Doe'}
-  // console.log('Hello');
-  return 'Hello ' + firstName + ' ' + lastName;
-}
-
-// console.log(greet());
-
-// FUNCTION EXPRESSIONS (puts a function as a variable assignment)
-
-const square = function(x){
-  return x*x;
-};
-
-// console.log(square(8));
-
-// IMMEDIATELY INVOKABLE FUNCTION EXPRESSIONS - IIFE's ( a function you declare & run at same time)
-
-// (function(){
-//   console.log('IIFE ran...');
-// })();
-
-// (function(name){
-//   console.log('Hello '+ name);
-// })('Person'); 
-
-// PROPERTY METHODS
-
-// when a function is put inside of an object, it's called a method.
-
-const todo = {
-  add: function(){
-    console.log('Add todo...');
-  },
-  edit: function(id){
-    console.log(`Edit todo ${id}`);
+for(let i = 0; i <= 10; i++){
+  if(i === 2){
+    console.log('2 is best');
+    continue;
   }
-}
 
-todo.delete = function(){
-  console.log('Delete todo...');
+  if(i === 10){
+    console.log('Stop the loop');
+    break;
+  }
+  
+  console.log('Number ' + i);
 }
-
-todo.add();
-todo.edit(22);
-todo.delete();
