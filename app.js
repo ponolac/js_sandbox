1c7be1c867fabf5f933eff81db17bdd182dc2e56
ponolac <ponolac@gmail.com>
2019-02-06 23:01:33 -0800

    while loop

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
-// FOR LOOP
+// // FOR LOOP - when you know how many iterations it will have.
 
-for(let i = 0; i <= 10; i++){
-  if(i === 2){
-    console.log('2 is best');
-    continue;
-  }
+// for(let i = 0; i <= 10; i++){
+//   if(i === 2){
+//     console.log('2 is best');
+//     continue;
+//   }
 
-  if(i === 10){
-    console.log('Stop the loop');
-    break;
-  }
+//   if(i === 10){
+//     console.log('Stop the loop');
+//     break;
+//   }
   
+//   console.log('Number ' + i);
+// }
+
+// WHILE LOOP - when iterations are unclear.
+
+let i = 0; 
+
+while(i < 10){
   console.log('Number ' + i);
+  i++;
 }
