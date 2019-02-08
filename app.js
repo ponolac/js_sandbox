ce56927296fe40478b4fc91ba93f528dd5a63516
ponolac <ponolac@gmail.com>
2019-02-07 17:27:29 -0800

    for..each loop, map method, for..in loop

--- app.js
+++ app.js
@@ -39,6 +39,45 @@
 const cars = ['Ford', 'Chevy', 'Honda', 'Toyota'];
 cars[1];
 
-for(let i = 0; i < cars.length; i++){
-  console.log(cars[i]);
+// for(let i = 0; i < cars.length; i++){
+//   console.log(cars[i]);
+// }
+
+// FOR EACH, LOOP THRU ARRAY
+
+// cars.forEach(function(car, index, array){ // takes in an anonymous function 
+//   console.log(`${index} : ${car}`);
+//   console.log(array);
+// });
+
+// MAP METHOD - most commmonly used to return a different array 
+
+// const users = [
+//   {id:1, name:'John'},
+//   {id:2, name:'Sara'},
+//   {id:3, name:'Tom'},
+//   {id:4, name:'Madeline'},
+// ];
+
+// const ids = users.map(function(user){
+//   return user.id;
+// });
+
+// console.log(ids);
+
+// FOR IN LOOP - often used for objects 
+
+ // looping thru object example. using for in loop.
+
+const user = {
+  firstName: 'John',
+  lastName: 'Johnson',
+  age: 30
 }
+
+for(let x in user){
+  console.log(`${x} : ${user[x]}`);
+}
+
+
+
