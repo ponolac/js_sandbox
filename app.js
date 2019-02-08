4faf1aa177ffea5087e3a160de78792ef68e4d0a
ponolac <ponolac@gmail.com>
2019-02-08 03:33:49 -0800

    scope: global, function, block

--- app.js
+++ app.js
@@ -1,57 +1,31 @@
-// WINDOW METHODS / OBJECTS / PROPERTIES
+// Global Scope
 
-// ALERT
+            // let and const have a block level scope
+            // var has a function scope
 
-// alert('Test'); // window.alert('Test');
+var a = 1;
+let b = 2;
+const c = 3;
 
-// PROMPT - similar to alert but takes in input
-
-// const input = prompt();
-// alert(input);
-
-// CONFIRM - used often when you're deleting something
-
-// if(confirm('Are you sure')){
-//   console.log('Yes');
-// } else {
-//   console.log('No');
+// function test() {
+//   var a = 4;
+//   let b = 5;
+//   const c = 6;
+//   console.log('Function Scope: ', a, b, c);
 // }
 
-let val;
+// test();
 
-// // OUTER HEIGHT AND WIDTH
-val = window.outerHeight;
-val = window.outerWidth;
+// if(true) {
+//   // Block Scope
+//   var a = 4;
+//   let b = 5;
+//   const c = 6;
+//   console.log('If Scope: ', a, b, c);
+// }
 
-// OUTER HEIGHT AND WIDTH;
-val = window.innerHeight;
-val = window.innerWidth;
+for(var a = 0; a < 10; a++) {
+  console.log(`Loop: ${a}`);
+}
 
-// SCROLL POINTS
-val = window.scrollY;
-val = window.scrollX;
-
-// LOCATION OBJECT - can be used to redirect and reload page.
-val = window.location;
-val = window.location.hostname;
-val = window.location.port;
-val = window.location.href;
-val = window.location.search;
-
- // Redirect
-//  window.location.href = 'http://google.com';
-
- // Reload
-// window.location.reload();
-
-// History Object
-// window.history.go(-1);
-// val = window.history.length; 
-
-// Navigator Object
-val = window.navigator;
-val = window.navigator.cookieEnabled;
-val = window.navigator.platform;
-val = window.navigator.geolocation;
-
-console.log(val);
+console.log('Global Scope: ', a, b, c);
