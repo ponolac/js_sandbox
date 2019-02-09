c166ca1f58189895a9ced1d6dcef9246162d016d
ponolac <ponolac@gmail.com>
2019-02-08 23:38:00 -0800

    change styling with getElementById

--- app.js
+++ app.js
@@ -3,3 +3,15 @@
 // document.getElementById()
 
 console.log(document.getElementById('task-title'));
+
+// Get things from the element
+
+console.log(document.getElementById('task-title').id);
+console.log(document.getElementById('task-title').className);
+
+// Change styling
+
+document.getElementById('task-title').style.background = '#333';
+document.getElementById('task-title').style.color = '#fff';
+document.getElementById('task-title').style.background = '#5px';
+// document.getElementById('task-title').style.display = 'none';
