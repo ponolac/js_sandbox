2beda32af23fb3ce755b9698efb463e3ebdfb555
ponolac <ponolac@gmail.com>
2019-02-08 23:45:07 -0800

    change content with getElementById

--- app.js
+++ app.js
@@ -15,3 +15,8 @@ document.getElementById('task-title').style.background = '#333';
 document.getElementById('task-title').style.color = '#fff';
 document.getElementById('task-title').style.background = '#5px';
 // document.getElementById('task-title').style.display = 'none';
+
+// Change content
+document.getElementById('task-title').textContent = 'Task List';
+document.getElementById('task-title').innerText = 'My Tasks';
+document.getElementById('task-title').innerHTML = '<span style = "color:red">Task List</span>';
