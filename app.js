ef1b2d4048124c2a55fcaf29eca98597109338ee
ponolac <ponolac@gmail.com>
2019-02-08 23:59:42 -0800

    query selector -- document.querySelector()

--- app.js
+++ app.js
@@ -6,19 +6,25 @@ console.log(document.getElementById('task-title'));
 
 // Get things from the element
 
-console.log(document.getElementById('task-title').id);
-console.log(document.getElementById('task-title').className);
+// console.log(document.getElementById('task-title').id);
+// console.log(document.getElementById('task-title').className);
 
-const taskTitle = document.getElementById('task-title');
+// const taskTitle = document.getElementById('task-title');
 
-// Change styling
+// // Change styling
 
-taskTitle.style.background = '#333';
-taskTitle.style.color = '#fff';
-taskTitle.style.padding = '5px';
-// taskTitle.style.display = 'none';
+// taskTitle.style.background = '#333';
+// taskTitle.style.color = '#fff';
+// taskTitle.style.padding = '5px';
+// // taskTitle.style.display = 'none';
 
-// Change content
-taskTitle.textContent = 'Task List';
-taskTitle.innerText = 'My Tasks';
-taskTitle.innerHTML = '<span style = "color:red">Task List</span>';
+// // Change content
+// taskTitle.textContent = 'Task List';
+// taskTitle.innerText = 'My Tasks';
+// taskTitle.innerHTML = '<span style = "color:red">Task List</span>';
+
+// document.querySelector()
+
+console.log(document.querySelector('#task-title'));
+console.log(document.querySelector('.card-title'));
+console.log(document.querySelector('h5'));
