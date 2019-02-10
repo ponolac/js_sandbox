d133d0d577f0f1228137d3848200aa23e8a2fe7f
ponolac <ponolac@gmail.com>
2019-02-09 23:59:44 -0800

    (continued) document.getElementsByClassName

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 // document.getElementsByClassName
 
 const items = document.getElementsByClassName('collection-item');
+console.log(items);
+console.log(items[0]);
+items[0].style.color = 'red';
+items[3].textContent = 'Hello';
