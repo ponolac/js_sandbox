6513c994f07777795259f052c9da4b6f69aea3cc
ponolac <ponolac@gmail.com>
2019-02-10 01:45:56 -0800

    document.querySelectorAll

--- app.js
+++ app.js
@@ -12,20 +12,41 @@
 
 // document.getElementsByTagName
 
-let lis = document.getElementsByTagName('li');
-console.log(lis);
-console.log(lis[0]);
-lis[0].style.color = 'red';
-lis[3].textContent = 'Harhar';
+// let lis = document.getElementsByTagName('li');
+// console.log(lis);
+// console.log(lis[0]);
+// lis[0].style.color = 'red';
+// lis[3].textContent = 'Harhar';
 
-// Convert HTML collection into an array
-lis = Array.from(lis);
+// // Convert HTML collection into an array
+// lis = Array.from(lis);
 
-lis.reverse(); // array method
+// lis.reverse(); // array method
 
-lis.forEach(function(li, index){
-  console.log(li.className);
-  li.textContent = `${index}: Hello`; // displays reverse because of                                             .reverse array method in line 24
+// lis.forEach(function(li, index){
+//   console.log(li.className);
+//   li.textContent = `${index}: Hello`; // displays reverse because of                                             .reverse array method in line 24
+// });
+
+// console.log(lis);
+
+// document.querySelectorAll
+
+const items = document.querySelectorAll('ul.collection li.collection-item');
+
+items.forEach(function(item, index){
+  item.textContent = `${index}: Hello`;
 });
 
-console.log(lis);
+const liOdd = document.querySelectorAll('li:nth-child(odd)');
+const liEven = document.querySelectorAll('li:nth-child(even)');
+
+liOdd.forEach(function(li, index){
+  li.style.background = '#ccc';
+});
+
+for(let i = 0; i < liEven.length; i++){
+  liEven[i].style.background = '#f4f4f4';
+}
+
+console.log(items);
