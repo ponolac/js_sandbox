cd24490b4c001fa5560f96d17e85870827aac385
ponolac <ponolac@gmail.com>
2019-02-10 21:55:14 -0800

    classes and attributes for removing and replacing elements in DOM

--- app.js
+++ app.js
@@ -28,3 +28,28 @@ lis[0].remove(); // call the remove method
 
 // Remove child element
 list.removeChild(lis[3]);
+
+// CLASSES & ATTRIBUTES
+
+const firstLi = document.querySelector('li:first-child');
+const link = firstLi.children[0];
+
+let val;
+
+// Classes
+val = link.className;
+val = link.classList;
+val = link.classList[0];
+link.classList.add('test');
+link.classList.remove('test');
+val = link;
+
+// Attributes
+val = link.getAttribute('href');
+val = link.setAttribute('href', 'http://google.com');
+link.setAttribute('title', 'Google');
+val = link.hasAttribute('title');
+link.removeAttribute('title');
+val = link;
+
+console.log(val);
