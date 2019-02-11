f44a8ed5446a6c955be086567195f39ec313b65b
ponolac <ponolac@gmail.com>
2019-02-10 21:41:15 -0800

    Replace / remove an element

--- app.js
+++ app.js
@@ -18,4 +18,13 @@ const cardAction = document.querySelector('.card-action');
 // Replace
 cardAction.replaceChild(newHeading, oldHeading);
 
-console.log(newHeading);
+// REMOVE ELEMENT
+
+const lis = document.querySelectorAll('li');
+const list = document.querySelector('ul');
+
+// Remove list item 
+lis[0].remove(); // call the remove method
+
+// Remove child element
+list.removeChild(lis[3]);
